aa751156920aa19e02f17d8c4a38dd7281c3d5c0
Akulation <akulationofficial@gmail.com>
2023-02-16 16:17:57 -0600

    added option to delete items when they are added to inventory

--- Config.cs
+++ Config.cs
@@ -5,9 +5,11 @@ namespace ItemRestrictions
     public class Config : IRocketPluginConfiguration
     {
         public string MessageColor { get; set; }
+        public bool DeleteItems { get; set; }
         public void LoadDefaults()
         {
             MessageColor = "yellow";
+            DeleteItems = false;
         }
     }
 }
--- ItemRestrictions.cs
+++ ItemRestrictions.cs
@@ -3,6 +3,7 @@ using Rocket.API;
 using Rocket.API.Collections;
 using Rocket.Core.Plugins;
 using Rocket.Unturned.Chat;
+using Rocket.Unturned.Events;
 using Rocket.Unturned.Player;
 using SDG.Unturned;
 using System.Collections.Generic;
@@ -22,6 +23,7 @@ namespace ItemRestrictions
             MessageColor = UnturnedChat.GetColorFromName(Configuration.Instance.MessageColor, Color.green);
             Logger.Log("ItemRestrictions has been successfully loaded!");
             ItemManager.onTakeItemRequested += onTakeItemRequested;
+            UnturnedPlayerEvents.OnPlayerInventoryAdded += OnPlayerInventoryAdded;
         }
 
         protected override void Unload()
@@ -32,6 +34,7 @@ namespace ItemRestrictions
 
         private void onTakeItemRequested(Player player1, byte x, byte y, uint instanceID, byte to_x, byte to_y, byte to_rot, byte to_page, ItemData itemData, ref bool shouldAllow)
         {
+            if (Configuration.Instance.DeleteItems) return;
             UnturnedPlayer player = UnturnedPlayer.FromPlayer(player1);
             if (System.IO.File.Exists("Plugins/ItemRestrictions/restrictions.json"))
             {
@@ -50,6 +53,28 @@ namespace ItemRestrictions
                         string itemName = Assets.find(EAssetType.ITEM, itemData.item.id).FriendlyName;
                         UnturnedChat.Say(player, Translate("ItemRestricted", itemName));
                         shouldAllow = false;
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void OnPlayerInventoryAdded(UnturnedPlayer player, Rocket.Unturned.Enumerations.InventoryGroup inventoryGroup, byte inventoryIndex, ItemJar P)
+        {
+            if (!Configuration.Instance.DeleteItems) return;
+            if (System.IO.File.Exists("Plugins/ItemRestrictions/restrictions.json"))
+            {
+                string fileContents = System.IO.File.ReadAllText("Plugins/ItemRestrictions/restrictions.json");
+                List<Restriction> restrictions = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Restriction>>(fileContents);
+                Restriction restriction = restrictions.FirstOrDefault(r => r.itemID == P.item.id);
+                if (restriction != null)
+                {
+                    if (player.HasPermission(restriction.PickupPermission)) return;
+                    else
+                    {
+                        string itemName = Assets.find(EAssetType.ITEM, P.item.id).FriendlyName;
+                        player.Inventory.removeItem((byte)inventoryGroup, inventoryIndex);
+                        UnturnedChat.Say(player, Translate("ItemRestricted", itemName));
                         return;
                     }
                 }
